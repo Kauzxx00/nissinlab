14f8db53fe680ab4a53f68adc1330439302acb15
Kauzxx00 <ed3ard0k13@gmail.com>
2025-01-23 19:22:46 -0300

    feat (login): Agora é possivel logar pelo discord

--- public/js/script.js
+++ public/js/script.js
@@ -6,10 +6,13 @@ const sections = Array.from(menuLinks).map(link => link.getAttribute("data-targe
 
 menuLinks.forEach((link, index) => {
     link.addEventListener("click", (e) => {
+        const isLogin = link.classList.contains("login");
+        if (isLogin) return; // Permite o comportamento normal do link de login
+
         e.preventDefault();
 
         const target = sections.indexOf(link.getAttribute("data-target"));
-        if (target === currentIndex) return;
+        if (target === -1 || target === currentIndex) return;
 
         menuLinks.forEach(link => link.classList.remove("active"));
         link.classList.add("active");
@@ -18,7 +21,6 @@ menuLinks.forEach((link, index) => {
         carousel.style.transform = `translateX(-${currentIndex * 100}vw)`;
     });
 });
-
 // ---- Menu Mobile ----
 const menuToggle = document.getElementById('menuToggle');
 const menu = document.getElementById('menu-mobile');
