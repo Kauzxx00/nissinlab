d64fae59a1094c83e1f832ee90c7f84f093526ea
Kauzxx00 <ed3ard0k13@gmail.com>
2025-01-22 23:02:14 -0300

    feat: Commit inicial

---
+++ script.js
@@ -0,0 +1,29 @@
+const menuLinks = document.querySelectorAll("nav a");
+const carousel = document.querySelector(".carousel-container");
+let currentIndex = 0;
+
+const sections = Array.from(menuLinks).map(link => link.getAttribute("data-target"));
+
+menuLinks.forEach((link, index) => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const target = sections.indexOf(link.getAttribute("data-target"));
+        if (target === currentIndex) return;
+
+        menuLinks.forEach(link => link.classList.remove("active"));
+        link.classList.add("active");
+
+        currentIndex = target;
+        carousel.style.transform = `translateX(-${currentIndex * 100}vw)`;
+    });
+});
+
+// ---- Menu Mobile ----
+const menuToggle = document.getElementById('menuToggle');
+const menu = document.getElementById('menu-mobile');
+
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
+});
